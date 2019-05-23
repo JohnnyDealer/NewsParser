d795067499a83326722d8ece24200f23b5591ab6
JohnnyDealer <Johnny_Dealer@mail.ru>
2019-05-23 17:18:44 +0300

    works normal

--- PycharmProjects/Parsing/Test.py
+++ PycharmProjects/Parsing/Test.py
@@ -3,32 +3,75 @@ import json
 from bs4 import BeautifulSoup
 # soup = BeautifulSoup(html_doc, 'html.parser')
 # print(soup.prettify())
+
+
+def get_anchors(page):
+    for i in range(1, page):
+        params = {'page': i}
+        request = requests.get(url, params)
+        soup = BeautifulSoup(request.text, 'html.parser')
+        my_anchors = soup.findAll("a", {"class": "doc-label doc-label--link doc-block__doc-label"})
+        return my_anchors
+
+
+def second_page_info(my_anchors):
+    for anchor in my_anchors:
+        new_request = requests.get(anchor.get('href'))
+        new_soup = BeautifulSoup(new_request.text, 'html.parser')
+        new_headers = new_soup.findAll("h1", {"class": "page__title page__title--small"})
+        new_anchors = new_soup.findAll("a", {"class": "doc-label doc-label--link list-bar__item"})
+        new_divs = new_soup.findAll("div", {"class": "relation-list__value"})
+        if len(my_anchors) == 1:
+            information['response'].insert(0,
+                {
+                    'title': str(new_headers[0].get_text()),
+                    'link': str(new_anchors[0].get('href')),
+                    'data': str(new_divs[-1].get_text()),
+                }
+            )
+        else:
+            information['response'].append(
+                {
+                    'title': str(new_headers[0].get_text()),
+                    'link': str(new_anchors[0].get('href')),
+                    'data': str(new_divs[-1].get_text()),
+                }
+            )
+        print(str(new_headers[0].get_text()))
+        print(str(new_anchors[0].get('href')))
+        print(str(new_divs[-1].get_text()))
+        with open('information.json', 'w') as file:
+            json.dump(information, file)
+
+
+def last_data():                   # Берёт последнюю дату со страниц
+    first_page = 'http://mep.mosreg.ru/dokumenty/normotvorchestvo?page=1'
+    first_page_request = requests.get(first_page)
+    first_page_soup = BeautifulSoup(first_page_request.text, 'html.parser')
+    first_page_spans = first_page_soup.findAll("span", {"class": "doc-block__date"})
+    last_date = str(first_page_spans[0].get_text())
+    return last_date
+
+
+def find_new(static_date):
+    date = last_data()
+    day, month, year = date[0:2], date[3:5], date[6:10]
+    static_day, static_month, static_year = static_date[0:2], static_date[3:5], static_date[6:10]
+    if year >= static_year:
+        if month >= static_month:
+            if day >= static_day:
+                a = get_anchors(2)
+                need_anchor = []
+                need_anchor.append(a[0])
+                second_page_info(need_anchor)
+                print("it works")
+            else:
+                print("nothing new")
+
+
 url = 'http://mep.mosreg.ru/dokumenty/normotvorchestvo/'
-LinksAndData = {}
-LinksAndData['response'] = []
-for i in range(1, 8):
-    params = {'page': i}
-    r = requests.get(url, params)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    mya = soup.findAll("a", {"class": "doc-label doc-label--link doc-block__doc-label"})
-    # lst = []
-    # lst = soup.find_all('a')
-    # finall = []
-    # data = []
-    for j in mya:
-        NewR = requests.get(j.get('href'))
-        NewSoup = BeautifulSoup(NewR.text, 'html.parser')
-        NewMyA = NewSoup.findAll("a", {"class": "doc-label doc-label--link list-bar__item"})
-        NewMyDiv = NewSoup.findAll("div", {"class": "relation-list__value"})
-        LinksAndData['response'].append(
-            {
-                'link': str(NewMyA[0].get('href')),
-                'data': str(NewMyDiv[-1].get_text()),
-            }
-        )
-        print(str(NewMyA[0].get('href')))
-        print(str(NewMyDiv[-1].get_text()))
-        # data.append(NewMyDiv[0].get_text())
-        # finall.append(NewMyA[0].get('href'))
-with open('information.json', 'w') as file:
-    json.dump(LinksAndData, file)
+information = {}
+information['response'] = []
+anchors = get_anchors(4)
+second_page_info(anchors)
+find_new("19.05.2019")
